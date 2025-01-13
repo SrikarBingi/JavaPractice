01ca21ac84a5e00a41549d7ddcb79b940a5d3462
Srikar Bingi <srikar.bingi@gmail.com>
2025-01-13 23:53:19 +0530

    multithreading

---
+++ Java/Day10/Callable.java
@@ -0,0 +1,22 @@
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        Callable<Integer> task = () -> {
+            int sum = 0;
+            for (int i = 1; i <= 10; i++) {
+                sum += i;
+            }
+            return sum;
+        };
+
+        ExecutorService executor = Executors.newSingleThreadExecutor();
+        Future<Integer> result = executor.submit(task);
+
+        System.out.println("Result: " + result.get()); // Blocking call
+        executor.shutdown();
+    }
+}
