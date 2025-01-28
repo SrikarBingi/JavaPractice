5f162f1e3121c7881433734d7eb93d44d0a2e08f
Srikar Bingi <srikar.bingi@gmail.com>
2025-01-28 21:33:13 +0530

    asdf

--- Java/Day1/a,java.java
+++ Java/Day1/a,java.java
@@ -1,31 +1,31 @@
-List<Integer> res = new ArrayList<>();
-		if(root==null){
-			return res;
-		}
-		Deque<TreeNode> deq = new LinkedList<>();
-		deq.add(root);
-		boolean leftToRight = true;
-		while(!deq.isEmpty()){
-			int size = deq.size();
-			for(int i=0;i<size;i++){
-				if(leftToRight){
-					TreeNode curr = deq.removeFirst();
-					res.add(curr.val);
+// List<Integer> res = new ArrayList<>();
+// 		if(root==null){
+// 			return res;
+// 		}
+// 		Deque<TreeNode> deq = new LinkedList<>();
+// 		deq.add(root);
+// 		boolean leftToRight = true;
+// 		while(!deq.isEmpty()){
+// 			int size = deq.size();
+// 			for(int i=0;i<size;i++){
+// 				if(leftToRight){
+// 					TreeNode curr = deq.removeFirst();
+// 					res.add(curr.val);
 
-					if(curr.left!=null) deq.addLast(curr.left);
-					if(deq.right!=null) deq.addLast(curr.right);
+// 					if(curr.left!=null) deq.addLast(curr.left);
+// 					if(deq.right!=null) deq.addLast(curr.right);
 
-					leftToRight = false;
-				}
-				else{
-					TreeNode curr = deq.removeLast();
-					res.add(curr.val);
+// 					leftToRight = false;
+// 				}
+// 				else{
+// 					TreeNode curr = deq.removeLast();
+// 					res.add(curr.val);
 
-					if(curr.left!=null) deq.addLast(curr.left);
-					if(deq.right!=null) deq.addLast(curr.right);
+// 					if(curr.left!=null) deq.addLast(curr.left);
+// 					if(deq.right!=null) deq.addLast(curr.right);
 
-					leftToRight = true;
-				}
-			}
-		}
-		return res;
+// 					leftToRight = true;
+// 				}
+// 			}
+// 		}
+// 		return res;
