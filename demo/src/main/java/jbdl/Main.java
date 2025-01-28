78815e3a15740adadcc6e623d93cd8d6aaf3f62f
Srikar Bingi <srikar.bingi@gmail.com>
2025-01-28 21:16:58 +0530

    asdfg

--- demo/src/main/java/jbdl/Main.java
+++ demo/src/main/java/jbdl/Main.java
@@ -3,6 +3,6 @@ package jbdl;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        System.out.println("Hey");
+        System.out.println("practice");
     }
 }
